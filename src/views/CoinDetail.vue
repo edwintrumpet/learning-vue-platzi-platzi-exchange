7886fcc72800bfc620e621c08b5e340ed756553f
Edwin Garcia <spark.com.co@gmail.com>
2020-10-30 07:19:15 -0500

    Problemas de reactividad

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -76,21 +76,52 @@
         :max="max"
         :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
       />
+
+      <h3 class="text-xl my-10">Mejores ofertas de cambio</h3>
+      <table>
+        <tr
+          v-for="m in markets"
+          :key="`${m.exchangeId}-${m.priceUsd}`"
+          class="border-b"
+        >
+          <td>
+            <b>{{ m.exchangeId }}</b>
+          </td>
+          <td>{{ m.priceUsd | dollar }}</td>
+          <td>{{ m.baseSymbol }} / {{ m.quoteSymbol }}</td>
+          <td>
+            <px-button
+              :is-loading="m.isLoading || false"
+              v-if="!m.url"
+              @custom-click="getWebSite(m)"
+            >
+              <slot>Obtener link</slot>
+            </px-button>
+            <a v-else class="hover:underline text-green-600" target="_blanck">{{
+              m.url
+            }}</a>
+          </td>
+        </tr>
+      </table>
     </template>
   </div>
 </template>
 
 <script>
 import api from "@/api";
+import PxButton from "@/components/PxButton";
 
 export default {
   name: "CoinDetail",
+
+  components: { PxButton },
 
   data() {
     return {
       asset: {},
       history: [],
-      isLoading: false
+      isLoading: false,
+      markets: []
     };
   },
 
@@ -119,15 +150,37 @@ export default {
   },
 
   methods: {
+    getWebSite(exchange) {
+      this.$set(exchange, "isLoading", true);
+
+      return api
+        .getExchange(exchange.exchangeId)
+        .then(res => {
+          // Esta sería la forma normal de asignar este valor,
+          // pero como el objeto ya existe y no tiene esta propiedad Vue no sabrá se fue modificado
+          // y no podrá actuar reactivamente a sus cambios
+          // exchange.url = res.exchangeUrl;
+          //
+          // Para que se actue reactivamente a los cambios de este valor se debe modificar así
+          this.$set(exchange, "url", res.exchangeUrl);
+        })
+        .finally(() => this.$set(exchange, "isLoading", false));
+    },
+
     getCoin() {
       const id = this.$route.params.id;
 
       this.isLoading = true;
 
-      Promise.all([api.getAsset(id), api.getAssetHistory(id)])
-        .then(([asset, history]) => {
+      Promise.all([
+        api.getAsset(id),
+        api.getAssetHistory(id),
+        api.getMarkets(id)
+      ])
+        .then(([asset, history, markets]) => {
           this.asset = asset;
           this.history = history;
+          this.markets = markets;
         })
         .finally(() => (this.isLoading = false));
     }
